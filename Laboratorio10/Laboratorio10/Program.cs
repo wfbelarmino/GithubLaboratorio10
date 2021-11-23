7bb7136645da2bd8b16408d37b84c7525cf2b10c
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-23 18:48:54 -0300

    Add files via upload

---
+++ Laboratorio10/Laboratorio10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Laboratorio10
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            TermometroLimite term = new TermometroLimite(35); 
+            Console.WriteLine(term.ToString());
+
+            term.LimiteSuperiorEvent += new TermometroLimite.MeuDelegate(TrataLimiteSuperior);
+            Console.Write("Temperatura: ");
+            Console.WriteLine(term.ToString());
+
+        }
+        private static void TrataLimiteSuperior(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+}
---
+++ Laboratorio10/Laboratorio10/Termometro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio10
+{
+    public class Termometro
+    {
+        private double valor;
+
+        public Termometro()
+        {
+            valor = 40.0;
+        }
+        public double Temperatura
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+        virtual public void Aumentar()
+        {
+            valor += 0.1;
+        }
+        virtual public void Aumentar(double quantia)
+        {
+            valor += quantia;
+        }
+        virtual public void Diminuir()
+        {
+            valor -= 0.1;
+        }
+        virtual public void Diminuir(double quantia)
+        {
+            valor -= quantia;
+        }
+        override public string ToString()
+        {
+            return valor.ToString();
+        }
+
+
+    }
+}
